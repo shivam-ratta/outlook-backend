e385cfc1f06723af9530e22eab0f4d9d02d75eed
Shivam Ratta <shivamratta212@gmail.com>
2023-12-15 21:26:07 +0530

    add cors

--- app.js
+++ app.js
@@ -35,8 +35,21 @@ const apiProxy = createProxyMiddleware('/oauth/token', {
 });
 
 // Use the proxy middleware for your API endpoint
-app.use('/oauth/token', apiProxy);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Change '*' to your specific origin if needed
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 app.use(bodyParser.json());
+app.use('/oauth/token', apiProxy);
 
 // Route to handle the equivalent of your cURL request
 app.post('/login', (req, res) => {
